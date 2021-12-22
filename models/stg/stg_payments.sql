8bcbebc2e18b0dea66508964c9ae0b593c8e8bf5
Mer Arnel Manahan <mermanahan@gmail.com>
2021-12-22 16:41:03 +0000

    refactored stg_payments model

--- models/stg/stg_payments.sql
+++ models/stg/stg_payments.sql
@@ -5,6 +5,15 @@
 }}
 
 with payment as (
-    select * from `dbt-tutorial.stripe.payment`
+    select 
+        id as payment_id
+         , orderid as order_id
+         , paymentmethod as payment_method
+         , status as payment_status
+         , amount
+         , created as created_at
+         , _batched_at
+    
+    from `dbt-tutorial.stripe.payment`
 )
 select * from payment
